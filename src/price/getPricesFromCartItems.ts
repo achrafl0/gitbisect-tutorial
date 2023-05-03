664a08ac9531e356bea1039eba84c3e9cf0f92b1
Achraf <63306573+alaamoum@users.noreply.github.com>
2023-05-03 13:03:48 +0200

    commit 14

---
+++ src/price/getPricesFromCartItems.ts
@@ -0,0 +1,12 @@
+import { Cart } from "../shop/cart";
+
+const itemPrices: Record<number, number> = {
+  1: 10,
+  2: 20,
+  3: 40,
+  4: 40,
+};
+
+export const getItemPrice = (itemId: number) => {
+  return Object.keys(itemPrices).includes(itemId.toString()) ? itemPrices[itemId] : 0;
+};
